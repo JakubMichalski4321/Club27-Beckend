27ecdba5d6075965a6881413356394cf1f6d522d
Jakub Michalski <65559531+JakubMichalski4321@users.noreply.github.com>
2021-03-30 14:01:10 +0200

    pass example database data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,13 @@ VALUES ('5e5e850b-8799-4b3b-8403-cfbde120261c', CURRENT_TIMESTAMP , 'XDDDDD', 'J
 
 INSERT INTO public.pajacyzm(id, created_date, content, author)
 VALUES ('5e5e850b-8799-4b3b-8403-cfbde120260c', CURRENT_TIMESTAMP , 'XDDDDD', 'Jackob');
+
+
+INSERT INTO public.mem(id, created_date, author, image_path, meme_likes, title)
+VALUES ('5e5e850b-8799-4b3b-8403-cfbde120210c', CURRENT_TIMESTAMP, 'Jackob', '/img/lol.jpg', 10, 'MyfirstMem');
+
+INSERT INTO public.mem(id, created_date, author, image_path, meme_likes, title)
+VALUES ('5e5e850b-8799-4b3b-8403-cfbde120240c', CURRENT_TIMESTAMP, 'Jackob', '/img/lol.jpg', 10, 'MyfirstMem');
+
+INSERT INTO public.mem(id, created_date, author, image_path, meme_likes, title)
+VALUES ('5e5e850b-8799-4b3b-8403-cfbde120270c', CURRENT_TIMESTAMP, 'Jackob', '/img/lol.jpg', 10, 'MyfirstMem');
