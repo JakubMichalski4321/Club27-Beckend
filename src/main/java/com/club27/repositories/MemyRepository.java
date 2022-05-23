8844b855a85653d2b77baa9501da76496cc235fd
Jakub Michalski <65559531+JakubMichalski4321@users.noreply.github.com>
2022-05-23 20:15:38 +0200

    Clean-up imports

--- src/main/java/com/club27/repositories/MemyRepository.java
+++ src/main/java/com/club27/repositories/MemyRepository.java
@@ -1,12 +1,9 @@
 package com.club27.repositories;
 
 import com.club27.domain.Mem;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
--- src/main/java/com/club27/repositories/PajacyzmyRepository.java
+++ src/main/java/com/club27/repositories/PajacyzmyRepository.java
@@ -1,12 +1,9 @@
 package com.club27.repositories;
 
 import com.club27.domain.Pajacyzm;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
