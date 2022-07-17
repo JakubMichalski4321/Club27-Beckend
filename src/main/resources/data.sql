2cac9bc1be4d475cba8fce5255563cbe6896a680
Jakub Michalski <65559531+JakubMichalski4321@users.noreply.github.com>
2022-07-17 17:04:13 +0200

    update sql-sample-data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,11 @@
 INSERT INTO user_account VALUES ('592f3321-1e9a-4ab8-98da-269189f0915c','2022-07-07 23:14:09.603','ania','$2a$10$KEH7ZNQY/XofMj4LmmKt6O7LmcPkRGNaaXAKhFerqYT/ix0FT6Nfq');
 INSERT INTO user_account VALUES ('7e021f83-ff51-4e80-9305-4211d9b8f19b','2022-07-04 23:27:34.407','jackob','$2a$10$RC25zEtS1FQAvFSuK3DBZOrnc5FwDDF6hl6.lfP5PDz1OSyyJbxi.');
+
+INSERT INTO dept VALUES ('3b10f85b-4b76-4598-81d2-a1b3e29d19dd', '2022-07-17 11:02:09.488', 0, 'dsadsadsad');
+
+INSERT INTO user_depts VALUES ('7e021f83-ff51-4e80-9305-4211d9b8f19b', '3b10f85b-4b76-4598-81d2-a1b3e29d19dd');
+INSERT INTO user_depts VALUES ('592f3321-1e9a-4ab8-98da-269189f0915c', '3b10f85b-4b76-4598-81d2-a1b3e29d19dd');
+
+INSERT INTO account_statement VALUES ('d4ffd713-e1e8-43c9-bb7c-fbba87657e49', '2022-07-15 13:06:43.693', 10, '7e021f83-ff51-4e80-9305-4211d9b8f19b', 'Był przepyszny', 'Za najlepszego kebsa w życiu', '3b10f85b-4b76-4598-81d2-a1b3e29d19dd');
+INSERT INTO account_statement VALUES ('d4ffd714-e1e8-43c9-bb7c-fbba87657e49', '2022-07-17 13:06:43.693', 10, '7e021f83-ff51-4e80-9305-4211d9b8f19b', 'Był przepyszny', 'Za najlepszego kebsa w życiu', '3b10f85b-4b76-4598-81d2-a1b3e29d19dd');
+INSERT INTO account_statement VALUES ('d4ffd715-e1e8-43c9-bb7c-fbba87657e49', '2022-07-16 13:06:43.693', 10, '7e021f83-ff51-4e80-9305-4211d9b8f19b', 'Był przepyszny', 'Za najlepszego kebsa w życiu', '3b10f85b-4b76-4598-81d2-a1b3e29d19dd');
